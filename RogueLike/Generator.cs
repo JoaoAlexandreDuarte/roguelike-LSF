9d9d614ecec6885473a3c245baa842258d13b11b
Leandro Brás <shadowgaming.7@gmail.com>
2018-06-20 21:01:54 +0100

    Começado fase 4, mudado .gitignore para manter a pasta de debug

--- RogueLike/Generator.cs
+++ RogueLike/Generator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,9 +12,24 @@ namespace RogueLike {
         // Generator Objects
         private World myWorld;
         public Map myMap;
+        private Trap myTrap;
 
         // Current Level
         private int level;
+
+        // Multiplier
+        private readonly float multiplier = 0.5f;
+        // Number of traps
+        private int baseTraps = 5;
+
+        // Get a random value for the Row and Column
+        int spawnRow;
+        int spawnCol;
+
+        // Files location
+        private readonly string localTrap = "Trap.txt";
+        private readonly string localFood = "Food.txt";
+        private readonly string localWeapon = "Weapon.txt";
 
         public Generator(int level, World myWorld, Random rnd) {
             this.rnd = rnd;
@@ -21,12 +37,42 @@ namespace RogueLike {
             this.myWorld = myWorld;
             myMap = new Map();
             PlaceTheMap();
+            PlaceTraps();
         }
 
         public void PlaceTheMap() {
             int spawnRow = (int)(rnd.NextDouble() * myWorld.Rows);
             int spawnCol = (int)(rnd.NextDouble() * myWorld.Columns);
             myWorld.myTiles[spawnRow, spawnCol].Insert(0, myMap);
+        }
+
+        public void PlaceTraps() {
+            List<string> TrapList = File.ReadAllLines(localTrap).ToList();
+
+            // Determine the max number of traps for this level
+            int maxTraps;
+            if (level < baseTraps) {
+                maxTraps = baseTraps + level;
+            } else {
+                maxTraps = baseTraps + (int)(level * multiplier);
+            }
+
+            baseTraps = rnd.Next(maxTraps);
+
+            for (int i = 0; i < baseTraps; i++) {
+                int randomTrap = rnd.Next(TrapList.Count());
+                string[] trap = TrapList[randomTrap].Split(' ');
+
+                myTrap = new Trap(trap[0], Convert.ToInt32(trap[1]));
+
+                do {
+                    spawnRow = (int)(rnd.NextDouble() * myWorld.Rows);
+                    spawnCol = (int)(rnd.NextDouble() * myWorld.Columns);
+                } while (myWorld.myTiles[spawnRow, spawnCol].Exit ||
+                myWorld.myTiles[spawnRow, spawnCol].AsPlayer);
+
+                myWorld.myTiles[spawnRow, spawnCol].Insert(0, myTrap);
+            }
         }
     }
 }
---
+++ RogueLike/Trap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RogueLike {
+    class Trap {
+        public string MyTrap { get; set; }
+        public int Damage { get; private set; }
+
+        public bool FallenInto { get; set; }
+
+        public Trap(string myTrap, int damage) {
+            MyTrap = myTrap;
+            Damage = damage;
+        }
+
+        public override string ToString() {
+            return "Trap (" + MyTrap.ToString() + ")";
+        }
+    }
+}
